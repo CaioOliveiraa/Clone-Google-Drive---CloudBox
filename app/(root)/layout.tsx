41dc39c1233a70ca958407a4883a9fd2c51e05f8
Caio Oliveira <caio2601@gmail.com>
2025-02-10 19:11:16 -0300

    feat: criação de componente dinamicos para visualizar os docuemntos

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -16,7 +16,7 @@ const Layout = async ({ children }: { children: React.ReactNode }) => {
             <Sidebar {...currentUser} />
             <section className='flex h-full flex-1 flex-col'>
                 <MobileNavigation {...currentUser} />
-                <Header userId={currentUser.id} accountId={currentUser.accountId}></Header>
+                <Header userId={currentUser.$id} accountId={currentUser.accountId}></Header>
                 <div className='main-content'>{children}</div>
             </section>
 
